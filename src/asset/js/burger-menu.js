0fb77b7c1cf14e5aa0e5a1c190d0c31fda3ed810
Johann six <six.johann@gmail.com>
2022-02-17 11:38:35 +0100

    🚧 need to fix menu burger

--- src/asset/js/burger-menu.js
+++ src/asset/js/burger-menu.js
@@ -1,9 +1,19 @@
-const btnMenu = document.querySelector('#menu-burger');
-const navPhone = document.querySelector('#navigator-phone');
-
+const header = document.querySelector('#header');
+const btnMenu = header.querySelector('#menu-burger');
+console.log(btnMenu);
+document.querySelector('#menu-burger').onclick = function(){
+    console.log('gg');
+}
+document.querySelector('.intro-btn').onclick = function(e){
+    e.preventDefault();
+    console.log('index.js');
+}
+btnMenu.onclick = ()=>{
+    console.log('test vue');
+}
 btnMenu.addEventListener('click', ()=>{
     console.log('header');
-    const navPhone = document.querySelector('#navigator-phone');
+    const navPhone = header.querySelector('#navigator-phone');
     navPhone.style.display = 'block';
     navPhone.style.visibility = 'visible';
     const iClose = document.createElement('i');
