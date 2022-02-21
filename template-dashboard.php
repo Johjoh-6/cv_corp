f8197ddad9a00baac0d72965f17917ac9cf88280
dimitrisarrazinkapeu <dimitri.sarrazin.pro.dev@gmail.com>
2022-02-21 14:26:09 +0100

    ajout de la partir dashboard avec scss

--- template-dashboard.php
+++ template-dashboard.php
@@ -6,9 +6,65 @@ get_header();
 
 can_access();
 ?>
+<section id="dashboard">
+    <div class="container_dash">
+        <div class="container_profil">
+            <h2>Journal de board</h2>
+                <div class="box_profil">
+                    <img src="" alt="ici photo du recruteur">
+                    <p>Recruteur chez : Bouygue</p>
+                    <p>Charger de Recrutement Tech</p>
+                </div>
+        </div>
+    </div>
+
+    <div class="visuel_profil">
+        <div class="box_recherche">
+            <form action="">
+                <div>
+                    <input class="cta_recherche" type="text" name="recherche" id="" value="Recherche un Candidats ">
+                    
+                </div>
+            </form>
+        </div>
+
+    <div class="container_candidat">
+            <div class="box_candidat">
+                <div class="box_candidat_1">
+                    <div class="candidat_picture">
+                        <img src="" alt="ici photo du candidat">
+                    </div>
+                        <div class="box_text_profil_candidat">
+                            <p>Nom :</p>
+                            <p>Prénom :</p>
+                            <p>Offret : Techiciens</p>
+                                <div class="cta_candit">
+                                    <a href="">Voir CV</a>
+                                </div>
+                        </div>
+                </div>
+            </div>
+            <div class="box_candidat">
+                <div class="box_candidat_1">
+                    <div class="candidat_picture">
+                        <img src="" alt="ici photo du candidat">
+                    </div>
+                        <div class="box_text_profil_candidat">
+                            <p>Nom :</p>
+                            <p>Prénom :</p>
+                            <p>Offret : Techiciens</p>
+                                <div class="cta_candit">
+                                    <a href="">Voir CV</a>
+                                </div>
+                        </div>
+                </div>
+            </div>
+
+    </div>
 
 
-<h1>dashboard</h1>
+    </div>
+</section>
 
 
 <?php get_footer();
