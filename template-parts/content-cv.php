6d09b2db07f64e18693eafc102d9a26283106103
Louis Cherik <louis.cherik@gmail.com>
2022-02-24 10:04:07 +0100

    design CV change

--- template-parts/content-cv.php
+++ template-parts/content-cv.php
@@ -72,71 +72,79 @@ $meta_user = get_user_meta($id_user);
 
 
     <section id="cvVisualizer">
-        <div class="fullCv relative flex">
+        <div class="fullCv relative">
             <?php if(is_user_logged_in() AND !empty($meta_user['img'][0])) { ?>
                 <img class="pfpCv" src="<?= wp_get_attachment_url($meta_user['img'][0]) ?>" alt="">
             <?php } ?>
-            <div class="cvLeft">
-                <ul>
-                    <li>
-                        <h2>{{ titleCv }}</h2>
-                    </li>
-                    <li>
-                        <label for="nom">Nom : </label>
-                        <span>{{ nomCv }}</span>
-                    </li>
-                    <li>
-                        <label for="prenom">Prénom : </label>
-                        <span>{{ prenomCv }}</span>
-                    </li>
-                    <li>
-                        <label for="email">Adresse mail : </label>
-                        <span>{{ emailCv }}</span>
-                    </li>
-                    <li>
-                        <label for="phone">Numéro de téléphone : </label>
-                        <span>{{ phoneCv }}</span>
-                    </li>
-                    <li>
-                        <label for="adresse">Adresse : </label>
-                        <span>{{ adresseCv }}</span>
-                    </li>
-                </ul>
+            <ul style="width: 100%;">
+                <li>
+                    <h2>{{ titleCv }}</h2>
+                </li>
+                <li>
+                    <label for="nom">Nom : </label>
+                    <span>{{ nomCv }}</span>
+                </li>
+                <li>
+                    <label for="prenom">Prénom : </label>
+                    <span>{{ prenomCv }}</span>
+                </li>
+                <li>
+                    <label for="email">Adresse mail : </label>
+                    <span>{{ emailCv }}</span>
+                </li>
+                <li>
+                    <label for="phone">Numéro de téléphone : </label>
+                    <span>{{ phoneCv }}</span>
+                </li>
+                <li>
+                    <label for="adresse">Adresse : </label>
+                    <span>{{ adresseCv }}</span>
+                </li>
+            </ul>
+            <div class="flex">
+                <div class="cvLeft">
 
-                <div class="formations">
-                    <h2>Formations</h2>
-                    <ul class="formationListCv">
 
-                    </ul>
-                </div>
-                <div class="langues">
-                    <h2>Langues</h2>
-                    <ul class="langueListCv">
 
-                    </ul>
-                </div>
-            </div>
-            <div class="cvRight">
 
-                <div class="experiences">
-                    <h2>Experiences</h2>
-                    <ul class="experienceListCv">
-                    </ul>
-                </div>
+                    <div class="skills">
+                        <h2>Compétences et savoir-être</h2>
+                        <ul class="skillListCv">
+                        </ul>
+                    </div>
 
-                <div class="skills">
-                    <h2>Compétences et savoir-être</h2>
-                    <ul class="skillListCv">
-                    </ul>
+                    <div class="langues">
+                        <h2>Langues</h2>
+                        <ul class="langueListCv">
+
+                        </ul>
+                    </div>
+
+                    <div class="hobbies">
+                        <h2>Loisirs</h2>
+                        <ul class="hobbyListCv">
+
+                        </ul>
+                    </div>
                 </div>
 
-                <div class="hobbies">
-                    <h2>Loisirs</h2>
-                    <ul class="hobbyListCv">
+                <div class="cvRight">
 
-                    </ul>
+                    <div class="experiences">
+                        <h2>Experiences</h2>
+                        <ul class="experienceListCv">
+                        </ul>
+                    </div>
+                    <div class="formations">
+                        <h2>Formations</h2>
+                        <ul class="formationListCv">
+
+                        </ul>
+                    </div>
+
+
+
                 </div>
-
             </div>
         </div>
     </section>
