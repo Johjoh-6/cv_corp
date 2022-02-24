b99e73ca3dfad9388ebe3b2a2bfe7cd1ab71d409
Johann six <six.johann@gmail.com>
2022-02-24 17:37:24 +0100

    need to change th search ajax

--- inc/ajax/ajax-dashboard.php
+++ inc/ajax/ajax-dashboard.php
@@ -4,8 +4,28 @@ add_action('wp_ajax_nopriv_ajax_dash_search', 'searchTheCvWithAjax');
 
 function searchTheCvWithAjax() {
     global $wpdb;
-    $allCvNew = $wpdb->get_results("SELECT * FROM cv_cv WHERE cv_title = '".$_POST['searchData']['value']."' OR user_firstname = '".$_POST['searchData']['value']."' OR user_lastname = '".$_POST['searchData']['value']."'");
-    $allCvNew = json_decode(json_encode($allCvNew), true);
+    $search = $_POST['search'];
+    $allCvNew = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}cv WHERE cv_title LIKE '%$search%' OR user_firstname LIKE '%$search%' OR user_lastname LIKE '%$search%'", ARRAY_A);
+    // $allCvNew = $wpdb->get_results("SELECT cv.id, cv.cv_title, cv.user_id, cv.user_firstname, cv.user_lastname, cv.user_email, cv.user_phone, cv.user_adress, cv.id_picture, cv.cv_title,experience.job_name, experience.company_name, experience.date_start, experience.date_end, experience.details ,  studies.study_name, studies.study_details, studies.school_name, studies.school_location, studies.date_start, studies.date_end, skills.skill_name, spivot.skill_level , langue.langue_name, lpivot.langue_level, hobbie.hobbie_name, hpivot.hobbie_details
+    // FROM {$wpdb->prefix}cv AS cv
+    // JOIN {$wpdb->prefix}experience AS experience
+    // ON experience.id_cv = cv.id
+    // JOIN {$wpdb->prefix}studies AS studies
+    // ON studies.id_cv = cv.id
+    // JOIN {$wpdb->prefix}skill_pivot AS spivot
+    // ON spivot.id_cv = cv.id
+    // JOIN {$wpdb->prefix}skills AS skills 
+    // ON spivot.id_skill = skills.id
+    // JOIN {$wpdb->prefix}langues_pivot AS lpivot
+    // ON lpivot.id_cv = cv.id
+    // JOIN {$wpdb->prefix}langues AS langue 
+    // ON lpivot.id_langue = langue.id
+    // JOIN {$wpdb->prefix}hobbie_pivot AS hpivot
+    // ON hpivot.id_cv = cv.id
+    // JOIN {$wpdb->prefix}hobbie AS hobbie 
+    // ON hpivot.id_hobbie = hobbie.id
+    // WHERE cv.cv_title LIKE '%$search%' OR cv.user_firstname LIKE '%$search%' OR cv.user_lastname LIKE '%$search%'", ARRAY_A);
+    
     $allCV = [];
     foreach ($allCvNew as $singleCv) {
         $id_user = $singleCv['id_user'];
